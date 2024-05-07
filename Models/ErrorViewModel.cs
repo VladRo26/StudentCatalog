c8d670e791ca9c6629b38df283c005513ffe1015
VladRo26 <100710098+VladRo26@users.noreply.github.com>
2024-05-07 12:42:57 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentCatalog.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
