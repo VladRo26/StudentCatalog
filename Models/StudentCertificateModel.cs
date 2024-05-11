64a9ca2aaebc5ef2ddc1245ce3ce33ef15fa4567
Ilie Octavian Tudor <92024989+Tudorr02@users.noreply.github.com>
2024-05-11 14:40:40 +0300

    ...

--- Models/StudentCertificateModel.cs
+++ Models/StudentCertificateModel.cs
@@ -7,16 +7,19 @@ namespace StudentCatalog.Models
     {
         public int Id { get; set; }
 
+
         [Required(ErrorMessage = "Reason for the certificate is required.")]
         public required string  Reason { get; set; }
 
         public int StudentId {  get; set; }
-        public required StudentModel Student { get; set; }
+        public StudentModel Student { get; set; }
 
         public required string FirstName { get; set; }
         public string LastName { get; set; }
         [RegularExpression(@"^07[0-9]{8}$", ErrorMessage = "Phone number must start with 07 and be 10 digits long")]
-        public required string PhoneNumber { get; set; }
+        public string PhoneNumber { get; set; }
         public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+       
     }
 }
