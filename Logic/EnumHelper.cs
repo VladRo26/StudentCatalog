3a74e410faae55d983ef40a7f13728485d9e94d7
VladRo26 <100710098+VladRo26@users.noreply.github.com>
2024-05-14 19:16:02 +0300

    modify roles and stuff

---
+++ Logic/EnumHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace StudentCatalog.Logic
+{
+    public static class EnumHelper
+    {
+        public static IEnumerable<SelectListItem> FilteredUserTypes()
+        {
+            var values = Enum.GetValues(typeof(UserType))
+                             .Cast<UserType>()
+                             .Where(e => e != UserType.UtilizatorNelogat && e != UserType.None)
+                             .Select(e => new SelectListItem
+                             {
+                                 Text = e.ToString(),
+                                 Value = ((int)e).ToString()
+                             });
+
+            return values;
+        }
+    }
+}
